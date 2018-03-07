b10b0beef02cac69277611eeda1fbe607c50713d
Raman Nareika <Raman.Nareika@thomsonreuters.com>
2018-03-08 01:54:11 +0300

    impl #2 with hack

--- src/index.js
+++ src/index.js
@@ -38,33 +38,32 @@ class SmartCalculator {
     let operations_stack = [];
     let priorities = new Map();
 
-    priorities.set('^', 1);
+    priorities.set('^', 3);
     priorities.set('*', 2);
     priorities.set('/', 2);
-    priorities.set('+', 3);
-    priorities.set('-', 3);
+    priorities.set('-', 1);
+    priorities.set('+', 1);
 
     expression.forEach(element => {
       if(!priorities.has(element)) {
         output_stack.push(element);
       }else {
-        if(operations_stack.length === 0) {
-          operations_stack.push(element);
-        } else {
-          let last_priority = priorities.get(operations_stack[operations_stack.length - 1]);
+        if(operations_stack.length > 0) {
+          let latest = operations_stack[operations_stack.length - 1];
           let current_priority = priorities.get(element);
 
-          if(current_priority >= last_priority){
+          while(current_priority <= priorities.get(latest) && current_priority !== 3) {
             output_stack.push(operations_stack.pop());
+            latest = operations_stack[operations_stack.length - 1];
           }
-          operations_stack.push(element);
         }
+
+        operations_stack.push(element);
       }
     });
-    let last = operations_stack.pop();
-    while(last) {
-      output_stack.push(last);
-      last = operations_stack.pop();
+    
+    while(operations_stack.length > 0) {
+      output_stack.push(operations_stack.pop());
     }
 
     return output_stack;
@@ -88,8 +87,7 @@ class SmartCalculator {
       if(Number.isInteger(current_element)) {
         stack.push(current_element);
       }else {
-        let right = stack.pop();
-        let left = stack.pop();
+        let [right, left] = [stack.pop(), stack.pop()];
         let res = operators.get(element)(left, right);
         
         stack.push(res);
